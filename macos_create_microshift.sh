de345e8359e4321fd78308198b9e3732c4f4e6e5
ksingh7 <karan.singh731987@gmail.com>
2022-04-12 17:49:10 +0530

    cleanup

--- macos_create_microshift.sh
+++ macos_create_microshift.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 
+start=$(date +'%s')
+
 echo "Creating Podman Machine..."
-podman machine init --cpus 4 --memory 6000
+podman machine init --cpus 4 --memory 6000 &> /dev/null
 
 echo "Starting Podman Machine..."
-podman machine start
+podman machine start &> /dev/null
 
 echo "Setting rootful mode..."
 podman machine set --rootful
 
 echo "Launching Microshift..."
-podman run -d --rm --name microshift --privileged -v microshift-data:/var/lib -p 6443:6443 -p 80:80 -p 8080:8080 quay.io/microshift/microshift-aio:latest
+podman run -d --rm --name microshift --privileged -v microshift-data:/var/lib -p 6443:6443 -p 80:80 -p 8080:8080 quay.io/microshift/microshift-aio:latest &> /dev/null
 echo "Waiting for Microshift to become Ready..."
 sleep 100
 default_dns_pod=$(podman exec microshift oc get po -A | grep -i dns-default | awk '{print $2}')
 podman exec microshift oc wait --for=condition=Ready --timeout=10m pod/$default_dns_pod -n openshift-dns
 
-echo "Microshift is Now Ready, and here are all running pods ..."
-podman exec microshift oc get po -A
+echo "Microshift is Now Ready ..."
 
 echo "Setting up OpenShift Web Console ..."
+echo "----------------------------------------------------------"
 podman exec microshift oc create -f https://raw.githubusercontent.com/ksingh7/microshift/main/01_openshift_console.yaml
-
-echo "Waiting for OpenShift Web Console to be ready ..."
 openshift_console_pod=$(podman exec microshift oc get po -A | grep -i openshift-console-deployment | awk '{print $2}')
-podman exec microshift oc wait --for=condition=Ready --timeout=10m pod/$openshift_console_pod -n openshift-console
+podman exec microshift oc wait --for=condition=Ready --timeout=10m pod/$openshift_console_pod -n kube-system
+echo "----------------------------------------------------------"
 
-echo "OpenShift Web Console is Now Ready, your Console URL is"
 url=$(podman exec microshift oc get route -n kube-system openshift-console -o jsonpath='{.spec.host}')
-echo "--------------------------------------------------------------------"
+echo "############### OpenShift Console is Ready ###############################"
 echo "http://$url"
-echo "--------------------------------------------------------------------"
+echo "##########################################################################"
 
-echo "To use oc/kubectl client, run the following command"
-echo "--------------------------------------------------------------------"
-echo "podman exec -it microshift /bin/bash"
-echo "--------------------------------------------------------------------"
+echo "Setting up OLM ..."
+echo "----------------------------------------------------------"
+podman exec microshift curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.20.0/install.sh -o install.sh &> /dev/null
+podman exec microshift chmod +x install.sh
+podman exec microshift ./install.sh v0.20.0 &> /dev/null
+echo "OLM Setup Completed ..."
+echo "############### Microshift Setup Completed ###############################"
+echo "OpenShift Console URL      : http://$url"
+echo "OpenShift / Kubectl Access : podman exec -it microshift /bin/bash"
+echo "##########################################################################"
